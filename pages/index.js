4630e2654f716ed13284e3cecdd42f90b58f3808
karamveergill3 <karamveergill3@gmail.com>
2026-05-07 17:19:31 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2042,7 +2042,7 @@ export default function Home() {
           collapsed={sidebarCollapsed}
           setCollapsed={setSidebarCollapsed}
         />
-        <div style={{ flex: 1, overflow: "hidden", display: "flex", flexDirection: "column", marginLeft: sidebarCollapsed ? 0 : 280, transition: "margin-left 0.28s cubic-bezier(0.4,0,0.2,1)" }}>
+        <div style={{ flex: 1, overflow: "hidden", display: "flex", flexDirection: "column" }}>
           <Arena
             user={user}
             profile={profile}
